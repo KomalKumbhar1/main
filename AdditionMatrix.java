a77ed76e4391b80e29329d7cb92cfc66d5e219f9
Komal Keshav Kumbhhar <komal1994kumbhar@gmail.com>
2022-03-24 13:57:33 +0530

    First Commit

---
+++ AdditionMatrix.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class AdditionMatrix {
+	
+	public static void main(String ar[])
+	{
+		int a[][]= {{1,2},{2,4}};
+		int b[][]= {{1,0},{3,4}};
+		//int c[][]=new int[2][2];
+		
+		for(int i=0;i<2;i++)
+		{
+			for(int j=0;j<2;j++)
+			{
+				//c[i][j]=a[i][j]+b[i][j];
+				//System.out.print(c[i][j]+" ");
+				
+				System.out.print(a[i][j]+b[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
